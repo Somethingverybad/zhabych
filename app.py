694decadaf754f6c7c82ddcae9343566879185f0
Андрей <Ancher001@yandex.ru>
2025-06-15 13:03:12 +0300

    Загадки

--- app.py
+++ app.py
@@ -9,6 +9,14 @@ def index():
 @app.route("/zg1/")
 def zagadka1():
      return render_template("zagadka1.html")
+     
+@app.route("/zg2/")
+def zagadka1():
+     return render_template("zagadka2.html")
+
+@app.route("/zg3/")
+def zagadka1():
+     return render_template("zagadka3.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
